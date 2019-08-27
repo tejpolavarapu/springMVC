a8ca3977486726514501b223f8bdaa43f9ba89a5
tejpolavarapu <tejpolavarapu@gmail.com>
2019-08-27 12:46:33 -0700

    added update and delete functionalities to controller

--- src/main/java/com/test/pluto/controller/PersonController.java
+++ src/main/java/com/test/pluto/controller/PersonController.java
@@ -5,7 +5,10 @@ import com.test.pluto.beans.dao.DatabaseSequence;
 import com.test.pluto.beans.dao.Person;
 import com.test.pluto.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,7 @@ import static com.test.pluto.constants.Constants.HELLO_MESSAGE;
 import static org.springframework.data.mongodb.core.FindAndReplaceOptions.options;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Update.update;
 
 @RequestMapping("/person")
 @Controller
@@ -100,17 +104,49 @@ public class PersonController {
 
     //insert a new person into db
     //URL-pattern :  http://localhost:9090/person/insert?fname={fname}&lname={lname}
+
+
+    //TODO auto increment id - reference : https://www.baeldung.com/spring-boot-mongodb-auto-generated-field
+
+/*    public long generateSequence(String seqName) {
+        DatabaseSequence counter = mongoOperation.findAndModify(query(where("_id").is(seqName)),
+                new Update().inc("seq",1), options().returnNew(true).upsert(true), DatabaseSequence.class);
+
+        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+    }*/
+
+    public long getNextSequenceId(String key) {
+
+        Query query = new Query(Criteria.where("_id").is(key));
+
+        Update update = new Update();
+        update.inc("seq", 1);
+
+        FindAndModifyOptions options = new FindAndModifyOptions();
+        options.returnNew(true);
+
+        DatabaseSequence seqId =
+                mongoOperation.findAndModify(query, update, options, DatabaseSequence.class);
+
+        return seqId.getSeq();
+
+    }
+
     @RequestMapping(value = "/insert", method= RequestMethod.GET)
     @ResponseBody
-    public String insertPerson(@RequestParam(value = "id", required=true) long id, @RequestParam(value = "fname", required=true) String firstName, @RequestParam(value = "lname", required=true) String lastName){
+    public String insertPerson(@RequestParam(value = "id",required = true) int id,@RequestParam(value = "fname", required=true) String firstName, @RequestParam(value = "lname", required=true) String lastName){
 
         Person person=new Person();
 
         String personList=null;
         //TODO replace the id with generateSequence
+        //person.setPersonId(getNextSequenceId(person.SEQUENCE_NAME));
         person.setPersonId(id);
+        System.out.println("<<<< Insert >>>>\nID - "+person.getPersonId());
         person.setFirstName(firstName);
+        System.out.println("First name - "+person.getFirstName());
         person.setLastName(lastName);
+        System.out.println("Last name - "+person.getLastName());
 
 
         mongoOperation.insert(person);
@@ -121,19 +157,49 @@ public class PersonController {
 
 
 
-    /*
-    //TODO auto increment id - reference : https://www.baeldung.com/spring-boot-mongodb-auto-generated-field
 
-    public long generateSequence(String seqName) {
-        DatabaseSequence counter = mongoOperation.findAndModify(query(where("_id").is(seqName)),
-                new Update().inc("seq",1), options().returnNew(true).upsert(true),
-                DatabaseSequence.class);
-        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+    //update a persons first name
+    //URL-pattern :  http://localhost:9090/person/insert?old_fname={fname}&new_fname={lname}
+    @RequestMapping(value = "/changefn", method= RequestMethod.GET)
+    @ResponseBody
+    public String changeFirstName(@RequestParam(value = "old_fname", required=true) String oldName, @RequestParam(value = "new_fname", required=true) String newName){
+
+        Person person=new Person();
+
+        String personList=null;
+
+        person=mongoOperation.findOne(query(where("firstName").is(oldName)), Person.class);
+
+        person.setFirstName(newName);
+        mongoOperation.save(person);
+
+        personList="Updated the person *"+oldName+"* to **"+newName+"**\n\n";
+        personList+=gson.toJson(person);
+        return personList;
+
+    }
+
+    //delete a persons by id
+    //URL-pattern :  http://localhost:9090/person/delete/{id}
+    @RequestMapping(value = "/delete/{personId}", method= RequestMethod.GET)
+    @ResponseBody
+    public String deleteById(@PathVariable("personId") String id){
+
+        Person person=new Person();
+
+        String personList=null;
+
+        mongoOperation.find(query(where("_id").is(id)),Person.class);
+        personList="Deleted the person "+ gson.toJson(person);
+        mongoOperation.remove(person);
+        return personList;
+
     }
 
 
 
-    */
+
+
 
 
 }
